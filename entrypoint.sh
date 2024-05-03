7ad0a5ce11a5cfe9c642e96346521d3a70cff215
Daniel Mota <manji.dm@gmail.com>
2024-05-03 17:34:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,28 @@
-#/bin/bash
+#!/bin/bash
 set -e
 
-# if keyword is found 
-if echo "$*" | grep -i -q FIXED;
+if [ -n "$GITHUB_EVENT_PATH" ];
 then
-  echo "Found keyword."
+    EVENT_PATH=$GITHUB_EVENT_PATH
+elif [ -f ./sample_push_event.json ];
+then
+    EVENT_PATH='./sample_push_event.json'
+    LOCAL_TEST=true
 else
-  echo "Nothing to process."
+    echo "No JSON data to process! :("
+    exit 1
+fi
+
+env
+jq . < $EVENT_PATH
+
+# if keyword is found
+if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*";
+then
+    # do something
+    echo "Found keyword."
+# otherwise
+else
+    # exit gracefully
+    echo "Nothing to process."
 fi
